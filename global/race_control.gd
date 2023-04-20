e3ca857200f97d62ea1a1bde288ad6e7b07513c3
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-20 23:22:03 +0300

    Setup menu now can change wheel/tire and drivetrain params

--- global/race_control.gd
+++ global/race_control.gd
@@ -9,6 +9,7 @@ var grid := []
 func _ready():
 	pass
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- scenes/vehicle/wheel_params.gd
+++ scenes/vehicle/wheel_params.gd
@@ -1,13 +1,11 @@
 class_name WheelSuspensionParameters
 extends Resource
 
-@export var tire_model: BaseTireModel
+@export var tire_model := BaseTireModel.new()
 @export var spring_length := 0.15
 @export var spring_stiffness := 45000.0
 @export var bump := 10000.0
 @export var rebound := 11000.0
-@export var wheel_mass := 20.0
-#@export var tire_radius := 0.3
-#@export var tire_width := 0.225
+@export var wheel_mass := 20.0 # Including brake disc and drive shaft
 @export var ackermann := 0.15
-@export var anti_roll := 50
+@export var anti_roll := 50.0
