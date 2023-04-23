86e4c02062c0adfbfe8abbcee8283e52c83dfad0
michurebmann <michurebmann@gmail.com>
2023-04-23 22:31:10 +0200

    corrected the draw size, enabled antialiasing, makes them bigger

--- scenes/gui/tire_data_app/tire_data_circle.gd
+++ scenes/gui/tire_data_app/tire_data_circle.gd
@@ -6,7 +6,9 @@ enum tire_pos {FL,FR,RL,RR}
 @export var tire_index:tire_pos = tire_pos.FL
 
 var radius_grip_ring:float = 0
-var tire_spin_abs:float = 0
+var tire_slip_z_abs:float = 0
+var tire_slip_x_abs:float = 0
+var tire_slip_max_abs:float = 0
 var max_scale_n:float = 0
 
 func _ready():
@@ -28,7 +30,9 @@ func _ready():
 func _process(delta):
 	if tire != null:
 		#clamp absolute spin from 0 to 1 to use in lerp function
-		tire_spin_abs = clamp(abs(tire.slip_vec.y), 0.0, 1.0)
+		tire_slip_z_abs = clamp(abs(tire.slip_vec.y), 0.0, 1.0)
+		tire_slip_x_abs = clamp(abs(tire.slip_vec.x), 0.0, 1.0)
+		tire_slip_max_abs = max(tire_slip_x_abs, tire_slip_z_abs)
 		
 		#calculate the radius of the indicator based on tire load
 		var gripScale = clamp((1.0/max_scale_n) * tire.y_force, 0.0, 1.0)
@@ -44,7 +48,7 @@ func _draw():
 	var colorG = Color(0.0, 1.0, 0.0)#Green
 	
 	# mix colors based on slip, green = tire has grip, red = tire has no grip
-	var color_mixed = colorG.lerp(colorR, tire_spin_abs)
+	var color_mixed = colorG.lerp(colorR, tire_slip_max_abs)
 	
 	# draw the circle sized based on actual load on tire
 	draw_circle_arc(center, radius_grip_ring, angle_from, angle_to, color_mixed)
@@ -52,17 +56,17 @@ func _draw():
 	# draw the line indicates lateral forces in newton
 	var origin_x_force = center
 	var target_x_force = origin_x_force + ((Vector2(61, 0) * (1.0/max_scale_n) * tire.force_vec.x)).rotated(-tire.rotation.y)
-	draw_line(origin_x_force, target_x_force, Color.DARK_RED, 1, false)
+	draw_line(origin_x_force, target_x_force, Color.DARK_RED, 1, true)
 	
 	# draw the line indicates longitudinal forces in newton
 	var origin_z_force = center
 	var target_z_force = origin_z_force + ((Vector2(0, 61) * (1.0/max_scale_n) * tire.force_vec.y)).rotated(-tire.rotation.y)
-	draw_line(origin_z_force, target_z_force, Color.DARK_BLUE, 1, false)
+	draw_line(origin_z_force, target_z_force, Color.DARK_BLUE, 1, true)
 	
 
 #code from examples in godot docs
 func draw_circle_arc(center, radius, angle_from, angle_to, color):
-	var nb_points = 16
+	var nb_points = 32
 	var points_arc = PackedVector2Array()
 
 	for i in range(nb_points + 1):
@@ -70,4 +74,4 @@ func draw_circle_arc(center, radius, angle_from, angle_to, color):
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 
 	for index_point in range(nb_points):
-		draw_line(points_arc[index_point], points_arc[index_point + 1], color, 1, false)
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color, 1, true)
