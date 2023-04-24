69d83a29fe6880ac145ef18daf031e7ab453daf9
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-24 11:34:34 +0300

    Update tire app to show temps

--- global/vehicle_api.gd
+++ global/vehicle_api.gd
@@ -19,3 +19,12 @@ func get_tire_wear():
 	var wear_br: float = car.wheel_br.tire_wear
 	
 	return [wear_fl, wear_fr, wear_bl, wear_br]
+
+
+func get_tire_temps():
+	var temp_fl: float = car.wheel_fl.tire_temp
+	var temp_fr: float = car.wheel_fr.tire_temp
+	var temp_bl: float = car.wheel_bl.tire_temp
+	var temp_br: float = car.wheel_br.tire_temp
+	
+	return [temp_fl, temp_fr, temp_bl, temp_br]
--- scenes/gui/tire_info_app.gd
+++ scenes/gui/tire_info_app.gd
@@ -1,12 +1,33 @@
 extends Control
 
+@onready var temp_colors := [$Panel/TireFL/TemperatureColor, $Panel/TireFR/TemperatureColor,
+							$Panel/TireBL/TemperatureColor, $Panel/TireBR/TemperatureColor]
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var wears: Array = VehicleAPI.get_tire_wear()
+	var temps: Array = VehicleAPI.get_tire_temps()
+	
 	
 	$Panel/TireFL/TireWear.text = "%3.2f %%" % (wears[0] * 100)
 	$Panel/TireFR/TireWear.text = "%3.2f %%" % (wears[1] * 100)
 	$Panel/TireBL/TireWear.text = "%3.2f %%" % (wears[2] * 100)
 	$Panel/TireBR/TireWear.text = "%3.2f %%" % (wears[3] * 100)
+	
+	for i in range(4):
+		var temp: float = temps[i]
+		if temp < 65:
+			var amount = clamp(temp / 65.0, 0, 1)
+			temp_colors[i].color = lerp(Color.BLUE, Color.LIME_GREEN, amount)
+		else:
+			var amount = clamp((temp-65) / (100.0-65), 0, 1)
+			temp_colors[i].color = lerp(Color.LIME_GREEN, Color.RED, amount)
+	
+	$Panel/TireFL/TemperatureColor/TemperatureLabel.text = "%2.2f" % temps[0]
+	$Panel/TireFR/TemperatureColor/TemperatureLabel.text = "%2.2f" % temps[1]
+	$Panel/TireBL/TemperatureColor/TemperatureLabel.text = "%2.2f" % temps[2]
+	$Panel/TireBR/TemperatureColor/TemperatureLabel.text = "%2.2f" % temps[3]
+	
+	
 
