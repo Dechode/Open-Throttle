80f3eb5f53e84f54a3f60bf9193319af82e0ed19
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-06-15 12:59:35 +0300

    Logic for finish lines and checkpoints, fix lap time to str conversion

--- scenes/checkpoint.gd
+++ scenes/checkpoint.gd
@@ -1,13 +1,14 @@
 class_name CheckPoint
 extends Node3D
 
-@export var is_finish_line := false
+@export var is_start_or_finish := false
 
 
 func _on_checkpoint_body_entered(body: Node3D) -> void:
-	if !body is BaseCar:
+	if not body is BaseCar:
 		return
 	
-	if is_finish_line:
+	if is_start_or_finish:
 		body.lap_timer.cross_finish_line()
-		
+	else:
+		body.lap_timer.cross_checkpoint()
--- scenes/lap_timer.gd
+++ scenes/lap_timer.gd
@@ -1,21 +1,30 @@
 class_name LapTimer
 extends Node
 
+signal lap_finished
+
 var last_lap_time_usec := 0
 var lap_start_time_usec := 0
 var lap := 0
 
+var checkpoints_done := 0
+
 
 func cross_finish_line():
-	var lap_time_usec := Time.get_ticks_usec() - lap_start_time_usec
-	last_lap_time_usec = lap_time_usec
-	
+	last_lap_time_usec = Time.get_ticks_usec() - lap_start_time_usec
 	lap_start_time_usec = Time.get_ticks_usec()
 	lap += 1
+	checkpoints_done = 0
+	
+	lap_finished.emit()
 
 
-func get_last_lap_time():
-	var msec: int = fmod(last_lap_time_usec / 1000000, 1000)
-	var sec: int = fmod(last_lap_time_usec / 1000000, 60)
-	var min: int = fmod(last_lap_time_usec / 1000000, 60 * 60) / 60
+func cross_checkpoint():
+	checkpoints_done += 1
+
+
+func get_lap_time_str(usec: int) -> String:
+	var msec: int = fmod(float(usec / 1000), 1000)
+	var sec: int = fmod(usec / 1000000, 60)
+	var min: int = fmod(usec / 1000000, 60 * 60) / 60
 	return "%02d:%02d:%03d" % [min, sec, msec]
