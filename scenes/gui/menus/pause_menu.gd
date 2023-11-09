c158fc3f310c606b82d14471129a2ce23d8a680f
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-11-09 13:15:30 +0200

    Connect button pressed signals, change button min sizes

--- scenes/gui/menus/pause_menu.gd
+++ scenes/gui/menus/pause_menu.gd
@@ -1,1 +1,21 @@
 extends Control
+
+
+func _on_resume_pressed() -> void:
+	self.hide()
+
+
+func _on_reset_car_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_goto_pits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/gui/menus/main_menu.tscn")
